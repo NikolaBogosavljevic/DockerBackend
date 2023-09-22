fe6836bea6aef8b8f0b3f89af3a64334c08514fc
Nikola Bogosavljevic <nikola.bogosavljevic@noseryoung.com>
2023-09-22 13:19:51 +0200

    added sonar

--- build/generated/sources/annotationProcessor/java/main/com/example/demo/domain/imagepost/dto/ImagePostMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/com/example/demo/domain/imagepost/dto/ImagePostMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-09-20T11:28:56+0200",
+    date = "2023-09-22T11:51:10+0200",
     comments = "version: 1.5.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.0.jar, environment: Java 17.0.6 (Oracle Corporation)"
 )
 @Component
--- build/generated/sources/annotationProcessor/java/main/com/example/demo/domain/role/dto/RoleMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/com/example/demo/domain/role/dto/RoleMapperImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-09-20T11:28:55+0200",
+    date = "2023-09-22T11:51:11+0200",
     comments = "version: 1.5.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.0.jar, environment: Java 17.0.6 (Oracle Corporation)"
 )
 @Component
